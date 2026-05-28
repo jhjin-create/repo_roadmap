71e16ab0ecc21f8e1bf5e7b17a0e0daccd2d37a5
jhjin-create <jhjin@hankyopa.com>
2026-05-28 10:36:56 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SebakDesigner from './sebak_designer_v55'
+
+function App() {
+  return (
+    <div style={{ margin: 0, padding: 0, boxSizing: 'border-box' }}>
+      <SebakDesigner />
+    </div>
+  )
+}
+
+export default App
